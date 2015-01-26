a1a2b0c0f488cf355460010bfed4d7d86416e18f
Mark McKenna <mark.denis.mckenna@gmail.com>
2015-01-25 22:44:15 -0400

    Trying to get testing to work

---
+++ test/src/test/java/UrlParserProcessorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author Mark McKenna &lt;mark.denis.mckenna@gmail.com&gt;
+ * @version 0.1
+ * @since 25/01/2015
+ */
+public class UrlParserProcessorTest {
+    @Test
+    public void baseTest() {
+        
+    }
+}
